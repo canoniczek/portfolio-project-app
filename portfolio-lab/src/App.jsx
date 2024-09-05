620856c0e57e3fa27e96ce2933a54798abf81e83
Aleksander Andruszkiewicz <aleksanderandruszkiewicz@MacBook-Air-Aleksander.local>
2024-09-05 06:58:11 +0200

    added first routing - test time

--- portfolio-lab/src/App.jsx
+++ portfolio-lab/src/App.jsx
@@ -1,18 +1,58 @@
 
-import './components/main.scss';
+// import './components/main.scss';
 
-// import './App.css'
+// // import './App.css'
+
+// function App() {
+
+
+//   return (
+//     <>
+//       <p className='remember'>test</p>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./Home.jsx";
+// import Middleware from "./Middleware.jsx";
+
+
 
 function App() {
-
-
   return (
-    <>
-      <p className='remember'>test</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/home" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+
+        
+    
+        <Route path="/sign-out" element={<SignOut />} /> */}
+
+
+        <Route path="/home" element={<Home />} />
+
+        {/* <Route path="" element={<Middleware />}>
+          <Route index element={<ShowFunds />} />
+          <Route path="/add-funds" element={<AddFunds />} />
+          <Route path="/show-funds" element={<ShowFunds />} />
+          <Route path="/main-panel" element={<MainPanel />} />
+          <Route path="/compare-values" element={<CompareValues />} />
+          <Route path="/investment-branches" element={<InvestmentBranches />} />
+          <Route path="/get-saving-history" element={<GetSavingHistory />} />
+          
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
-
-
+export default App;
--- portfolio-lab/src/Home.jsx
+++ portfolio-lab/src/Home.jsx
@@ -1,5 +1,6 @@
 
 
+import './components/main.scss';
 
 
 
